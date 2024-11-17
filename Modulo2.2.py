44c012c876ded318bc5e37a12ce2549f149bd136
Arias-GRJ <Jeidenarias@gmail.com>
2024-11-16 20:36:24 -0400

    Update Modulo2.2.py

---
+++ Modulo2.2.py
@@ -0,0 +1,8 @@
+# Solicitar la temperatura en Fahrenheit al usuario
+fahrenheit = float(input("Introduce la temperatura en Fahrenheit: "))
+
+# Calcular la temperatura en Celsius
+celsius = (fahrenheit - 32) * 5/9
+
+# Mostrar el resultado
+print("La temperatura en Celsius es:", celsius)
