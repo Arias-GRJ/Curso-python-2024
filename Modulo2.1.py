27fd4d0ca991541c4bd4ffd61dffd08c3e12710e
Arias-GRJ <Jeidenarias@gmail.com>
2024-11-16 20:31:04 -0400

    Add files via upload

---
+++ Modulo2.1.py
@@ -0,0 +1,8 @@
+# Solicitar la longitud en metros al usuario
+metros = float(input("Introduce la longitud en metros: "))
+
+# Convertir metros a pulgadas (1 metro = 39.3701 pulgadas)
+pulgadas = metros * 39.3701
+
+# Mostrar el resultado
+print("La longitud en pulgadas es:", pulgadas)
